5780eeefc0692db0dce23e72b794e59ad3a0b539
vuthituyetmai1804 <vuthituyetmai1804@gmail.com>
2026-05-19 14:49:29 +0700

    3_Kiem_thu_mo_hinh.py

---
+++ pages/3_Kiem_thu_mo_hinh.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("📈 Kiểm thử mô hình")
+st.metric("Accuracy", "88%")
+st.metric("Recall (Quan trọng nhất)", "82%")
+
+st.write("### Confusion Matrix")
+# st.image("confusion_matrix.png")
