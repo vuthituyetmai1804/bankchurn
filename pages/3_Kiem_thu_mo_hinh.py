3c441cf05932a4444d9a918b37abf87502fa62d4
vuthituyetmai1804 <vuthituyetmai1804@gmail.com>
2026-05-19 14:56:38 +0700

    Delete pages directory

--- pages/3_Kiem_thu_mo_hinh.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📈 Kiểm thử mô hình")
-st.metric("Accuracy", "88%")
-st.metric("Recall (Quan trọng nhất)", "82%")
-
-st.write("### Confusion Matrix")
-# st.image("confusion_matrix.png")
