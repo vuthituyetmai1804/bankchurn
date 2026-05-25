da6f6a5050a7f872e7817f5739fa2c9cab793023
vuthituyetmai1804 <vuthituyetmai1804@gmail.com>
2026-05-26 02:25:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,12 +72,10 @@ st.markdown('<div class="wave-container"></div>', unsafe_allow_html=True)
 # =========================================================
 # UI HEADER
 # =========================================================
-col_logo_l, col_logo_m, col_logo_r = st.columns([1, 2, 1])
-with col_logo_m:
-    st.image("https://upload.wikimedia.org/wikipedia/commons/f/f4/Logo_Bidv_m%E1%BB%9Bi.svg", width=300)
-
-# Tiêu đề và dòng mô tả
 st.markdown("""
+<div style="display: flex; justify-content: center; align-items: center; margin-bottom: 10px;">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/Logo_Bidv_m%E1%BB%9Bi.svg" width="300">
+</div>
 <div style="text-align: center; margin-bottom: 30px;">
     <h2 style="color: #007353; font-weight: 800; margin-bottom: 5px;">🏦 HỆ THỐNG DỰ ĐOÁN KHÁCH HÀNG RỜI BỎ</h2>
     <p style="font-size: 17px; color: #555; font-style: italic; font-weight: 300;">
