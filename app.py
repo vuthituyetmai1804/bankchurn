e1d6aef0f179fe82bd152a217099104698d6adcd
vuthituyetmai1804 <vuthituyetmai1804@gmail.com>
2026-05-26 00:11:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -200,18 +200,19 @@ if predict_btn:
     # =====================================================
     # RIGHT PANEL
     # =====================================================
-                html_code = f"""
-                <div class="ai-result-card">
-                    <h2 style="color:white;text-align:center;">KẾT QUẢ DỰ ĐOÁN</h2>
-                    <div class="ai-circle" style="border-color:{glow}; box-shadow:0 0 20px {glow};">
-                        <div class="ai-percent">{risk_percent}%</div>
-                    </div>
-                    <div class="ai-risk-title">{risk_name}</div>
-                    <div class="ai-sub">Khách hàng có khả năng rời bỏ dịch vụ</div>
-                    <div class="ai-mini-card">
-                        <div class="ai-mini-title">🎯 Khuyến nghị hành động</div>
-                        <div class="ai-mini-content">{recommendation}</div>
-                    </div>
-                </div>
-                """
-                st.markdown(html_code, unsafe_allow_html=True)
+    with right_panel:    
+        html_code = f"""
+        <div class="ai-result-card">
+            <h2 style="color:white;text-align:center;">KẾT QUẢ DỰ ĐOÁN</h2>
+            <div class="ai-circle" style="border-color:{glow}; box-shadow:0 0 20px {glow};">
+                <div class="ai-percent">{risk_percent}%</div>
+            </div>
+            <div class="ai-risk-title">{risk_name}</div>
+            <div class="ai-sub">Khách hàng có khả năng rời bỏ dịch vụ</div>
+            <div class="ai-mini-card">
+                <div class="ai-mini-title">🎯 Khuyến nghị hành động</div>
+                <div class="ai-mini-content">{recommendation}</div>
+            </div>
+        </div>
+        """
+        st.markdown(html_code, unsafe_allow_html=True)
